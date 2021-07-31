de273fa98cc11d37766e68d649d9dc4b48b3d0b4
bobo9245 <bobo9245@naver.com>
2021-07-31 13:19:20 +0900

    first

---
+++ 애니메이션(React)/animation-react/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+
+
+function Box() {
+  return <div class="app-index-box" value='10'>
+    Press this for 
+  </div>;
+}
+
+function box_Number(){
+
+}
+
+export default Box;
