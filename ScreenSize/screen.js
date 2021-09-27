f0146efacaab9cf15bb247e53a829f5e5311b6ad
bobo9245 <bobo9245@naver.com>
2021-09-27 11:25:28 +0900

    Third commit : add ScreenSize.page

---
+++ ScreenSize/screen.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("button");
+
+btn.addEventListener("click",function(){
+    // alert(`${window.innerHeight}` + ' , ' + `${window.innerWidth}`);
+    document.write("<h1>`${window.innerHeight}` + ' , ' + `${window.innerWidth}`</h1>");
+})
